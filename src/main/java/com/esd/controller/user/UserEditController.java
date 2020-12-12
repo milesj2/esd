0b6a8e08ef83f55ffcf05d472ef27ab2a6120482
mi2-jarvis <miles2.jarvis@live.uwe.ac.uk>
2020-12-12 10:39:32 +0000

    14 - User Management Console
    Minor review changes

--- src/main/java/com/esd/controller/user/UserEditController.java
+++ src/main/java/com/esd/controller/user/UserEditController.java
@@ -49,68 +49,63 @@ public class UserEditController extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, java.io.IOException
     {
-
-        boolean updateUserResult;
-        boolean updateUserDetailsResult;
-
         // Validate user is logged in and admin
         User currentUser = (User)(request.getSession().getAttribute("currentSessionUser"));
         if(currentUser == null){
-            // out.print("Redirects are disabled in your browser please enable them to continue");
             response.sendRedirect("../index.jsp");
             return;
         } else if (currentUser.getUserGroup() != UserGroup.ADMIN){
-            // out.print("Redirects are disabled in your browser please enable them to continue");
             response.sendRedirect("../index.jsp");
             return;
         }
 
+        boolean active = false;
+        if (request.getParameter(DaoConsts.SYSTEMUSER_ACTIVE) != null)
+            active = true;
 
+        User user = new User(
+                Integer.parseInt(request.getParameter(DaoConsts.SYSTEMUSER_ID)),
+                request.getParameter(DaoConsts.SYSTEMUSER_USERNAME),
+                request.getParameter(DaoConsts.SYSTEMUSER_PASSWORD),
+                UserGroup.valueOf(request.getParameter(DaoConsts.SYSTEMUSER_USERGROUP)),
+                active
+        );
 
-            boolean active = false;
-            if (request.getParameter(DaoConsts.SYSTEMUSER_ACTIVE) != null)
-                active = true;
+        UserDetails userDetails = new UserDetails(
+                -1,
+                user.getId(),
+                request.getParameter(DaoConsts.USERDETAILS_FIRSTNAME),
+                request.getParameter(DaoConsts.USERDETAILS_LASTNAME),
+                request.getParameter(DaoConsts.USERDETAILS_ADDRESS1),
+                request.getParameter(DaoConsts.USERDETAILS_ADDRESS2),
+                request.getParameter(DaoConsts.USERDETAILS_ADDRESS3),
+                request.getParameter(DaoConsts.USERDETAILS_TOWN),
+                request.getParameter(DaoConsts.USERDETAILS_POSTCODE),
+                request.getParameter(DaoConsts.USERDETAILS_DOB)
+        );
 
-            User user = new User(
-                    Integer.parseInt(request.getParameter(DaoConsts.SYSTEMUSER_ID)),
-                    request.getParameter(DaoConsts.SYSTEMUSER_USERNAME),
-                    request.getParameter(DaoConsts.SYSTEMUSER_PASSWORD),
-                    UserGroup.valueOf(request.getParameter(DaoConsts.SYSTEMUSER_USERGROUP)),
-                    active
-            );
+        // Try updating SQL
+        try {
+            boolean updateUserResult;
+            boolean updateUserDetailsResult;
 
-            UserDetails userDetails = new UserDetails(
-                    -1,
-                    user.getId(),
-                    request.getParameter(DaoConsts.USERDETAILS_FIRSTNAME),
-                    request.getParameter(DaoConsts.USERDETAILS_LASTNAME),
-                    request.getParameter(DaoConsts.USERDETAILS_ADDRESS1),
-                    request.getParameter(DaoConsts.USERDETAILS_ADDRESS2),
-                    request.getParameter(DaoConsts.USERDETAILS_ADDRESS3),
-                    request.getParameter(DaoConsts.USERDETAILS_TOWN),
-                    request.getParameter(DaoConsts.USERDETAILS_POSTCODE),
-                    request.getParameter(DaoConsts.USERDETAILS_DOB)
-            );
+            updateUserResult = UserService.getInstance().updateUser(user);
+            updateUserDetailsResult = UserDetailsService.getInstance().updateUserDetails(userDetails);
 
-            // Try updating SQL
-            try {
-                updateUserResult = UserService.getInstance().updateUser(user);
-                updateUserDetailsResult = UserDetailsService.getInstance().updateUserDetails(userDetails);
-
-                if (updateUserResult && updateUserDetailsResult){
-                    response.sendRedirect("manage?errMsg=Success");
-                }
-                else {
-                    response.sendRedirect("manage?errMsg=Error updating user or user details.");
-                }
-
+            if (updateUserResult && updateUserDetailsResult){
+                response.sendRedirect("manage?errMsg=Success");
             }
-            catch (SQLException e){
-                response.sendRedirect("manage?errMsg=" + e.getMessage());
-                System.out.println(e.getMessage());
-            } catch (InvalidIdValueException e){
-                response.sendRedirect("manage?errMsg=" + "Invalid id user'" + user.getId() + "'");
-                System.out.println(e.getMessage());
+            else {
+                response.sendRedirect("manage?errMsg=Error updating user or user details.");
             }
+
+        }
+        catch (SQLException e){
+            response.sendRedirect("manage?errMsg=" + e.getMessage());
+            System.out.println(e.getMessage());
+        } catch (InvalidIdValueException e){
+            response.sendRedirect("manage?errMsg=" + "Invalid id user'" + user.getId() + "'");
+            System.out.println(e.getMessage());
+        }
     }
 }
