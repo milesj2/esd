5e27c761e12461de1b3562c41691fb530521f976
t3e1er <trentmeier1@gmail.com>
2021-01-20 15:30:23 +0000

    feature/78 page title null check and nav links reversion

--- src/main/java/com/esd/model/data/UserGroup.java
+++ src/main/java/com/esd/model/data/UserGroup.java
@@ -18,27 +18,4 @@ public enum UserGroup {
     ALL;
 
     public static List<UserGroup> patients = Arrays.asList(NHS_PATIENT, PRIVATE_PATIENT);
-
-    public static Map<String, Object> getNavForUserGroup(UserGroup userGroup){
-
-        Map<String, Object> navMap = new HashMap<>();
-            Map<String, String> subNavList = new HashMap<>();
-            subNavList.put("Book An Appointment", "/appointment/schedule");
-            subNavList.put("View Appointments", "/appointment/view");
-        navMap.put("Appointments", subNavList);
-        navMap.put("Invoices", "/invoices/search");
-
-        if(userGroup.equals(UserGroup.DOCTOR) || userGroup.equals(UserGroup.NURSE) ||
-                userGroup.equals(UserGroup.ADMIN) || userGroup.equals(UserGroup.RECEPTIONIST)){
-            subNavList.clear();
-                subNavList.put("Search Users", "/users/search");
-            navMap.put("Users", subNavList);
-        }
-
-        if(userGroup.equals(UserGroup.ADMIN) || userGroup.equals(UserGroup.ADMIN)){
-
-        }
-
-        return navMap;
-    }
 }
