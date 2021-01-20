d43f890459fc665daca3fce55be28528cebc83ab
t3e1er <trentmeier1@gmail.com>
2021-01-20 03:16:40 +0000

    feature/77 pdf report controller

--- src/main/java/com/esd/controller/pagecontrollers/pdf/ReportPdfController.java
+++ src/main/java/com/esd/controller/pagecontrollers/pdf/ReportPdfController.java
@@ -1,10 +1,7 @@
 package com.esd.controller.pagecontrollers.pdf;
 
-import com.esd.model.dao.DaoConsts;
-import com.esd.model.data.persisted.Prescription;
-import com.esd.model.data.persisted.UserDetails;
-import com.esd.model.service.PrescriptionService;
 import com.esd.model.service.UserDetailsService;
+import com.esd.model.service.reports.SystemOverViewReportService;
 import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
@@ -15,6 +12,7 @@ import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.property.HorizontalAlignment;
 import com.itextpdf.layout.property.TextAlignment;
+import org.joda.time.LocalDate;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -23,9 +21,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-@WebServlet("/prescriptions/pdf")
-public class PrescriptionPdfController extends HttpServlet {
+@WebServlet("/admin/reports/pdf")
+public class ReportPdfController extends HttpServlet {
 
     private UserDetailsService userDetailsService = UserDetailsService.getInstance();
 
@@ -33,18 +34,17 @@ public class PrescriptionPdfController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        try{
-            //get invoice
-            Prescription prescription = PrescriptionService.getInstance().getPrescriptionById(Integer.parseInt(request.getParameter(DaoConsts.ID)));
-            UserDetails userDetails = userDetailsService.getUserDetailsByUserID(prescription.getPatientId());
-            UserDetails employee = userDetailsService.getUserDetailsByID(prescription.getEmployeeId());
+        try {
+            LocalDate startDate = LocalDate.parse(request.getParameter("startDate"));
+            LocalDate endDate = LocalDate.parse(request.getParameter("endDate"));
+            HashMap<String, String> reportValMap = SystemOverViewReportService.getInstance().getReportData(startDate, endDate);
 
             response.setContentType("application/pdf");
             PdfDocument pdfDoc = new PdfDocument(new PdfWriter(response.getOutputStream()));
 
             Document doc = new Document(pdfDoc);
             //set margin
-            doc.setMargins(60, 40, 60, 40);
+            doc.setMargins(40, 40, 40, 40);
 
             //get logo
             //Image logo = new Image(ImageDataFactory.create(getServletContext().getContextPath()+"/res/images/logo.png"));
@@ -53,37 +53,24 @@ public class PrescriptionPdfController extends HttpServlet {
             logo.setHorizontalAlignment(HorizontalAlignment.RIGHT);
             doc.add(logo);
 
-            //add content
-            doc.add(new Paragraph("SMARTWARE\n \n" +
-                    "Perscription For:" + userDetails.getFirstName()+" "+
-                    userDetails.getLastName()+"\n"+
-                    userDetails.getAddressLine1()+"\n"+
-                    userDetails.getTown()+"\n"+
-                    userDetails.getPostCode()+"\n \n"
+            doc.add(new Paragraph("SMARTWARE SYSTEM REPORTS\n \n" +
+                    "Report Start Date: " + startDate + "\n"+
+                    "Report End Date: " + endDate + "\n \n"
             ).setTextAlignment(TextAlignment.LEFT));
-
-            doc.add(new Paragraph("INVOICE")).setTextAlignment(TextAlignment.RIGHT);
 
             //create table
             float[] columnWidths = {240f, 240f};
             Table table = new Table(columnWidths);
-            //first column
-            table.addCell(new Cell().add("Date"));
-            table.addCell(new Cell().add(prescription.getIssueDate().toString()));
-            table.addCell(new Cell().add("Prescription Issuing Doctor"));
-            table.addCell(new Cell().add(employee.getFirstName()+ " "+employee.getLastName()));
-            table.addCell(new Cell().add("Prescription Details"));
-            table.addCell(new Cell().add(prescription.getPrescriptionDetails()));
-            table.addCell(new Cell().add("Prescription  Issuing Doctor"));
-            table.addCell(new Cell().add(employee.getFirstName()+ " "+employee.getLastName()));
-            table.addCell(new Cell().add("Originating Date"));
-            table.addCell(new Cell().add(prescription.getOriginatingPrescriptionId().toString()));
 
-            table.setHorizontalAlignment(HorizontalAlignment.CENTER);
+            Iterator reportValIterator = reportValMap.entrySet().iterator();
+            while(reportValIterator.hasNext()){
+                Map.Entry vals = (Map.Entry)reportValIterator.next();
+                table.addCell(new Cell().add(vals.getKey().toString()));
+                table.addCell(new Cell().add(vals.getValue().toString()));
+                reportValIterator.remove();
+            }
+
             doc.add(table);
-
-            doc.add(new Paragraph("\nPlease Submit to SMARTCARE Pharmacy for Prescription")
-            ).setTextAlignment(TextAlignment.LEFT);
             doc.close();
 
         } catch (Exception e) {
